5fbec8adc0beb706f92a81848ef916fcbf49ff13
Ganesh Doppani <ganesh.doppani@gmail.com>
2023-03-31 11:08:43 +0530

    [Ganesh] Add:Mood analyzer file is added

---
+++ MAnalyzer/MoodAnalyzer.cs
@@ -0,0 +1,10 @@
+﻿namespace MAnalyzer
+{
+    public class MoodAnalyzer
+    {
+        public void AnalyzeMood()
+        {
+            Console.WriteLine("Predicting mood by a message");
+        }
+    }
+}
